874e5956ea0ce36b39e5751111fc77e81bd78403
Emily Pellini <pellini.emily@gmail.com>
2022-03-26 16:17:54 -0300

    Commit 3

--- Laço de Repetição e Array/Média Números Múltiplos de 3.java
+++ Laço de Repetição e Array/Média Números Múltiplos de 3.java
@@ -7,39 +7,36 @@ public class ListaDoisExercícioSeis {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int numerodoteclado = 1, soma = 0, media = 0;
+		int numerodoteclado=0;
+		int soma = 0;
+		int media = 0;
 		int contanumeros = 0;
 
 		Scanner leia = new Scanner(System.in);
 
-		while (numerodoteclado != 0) {
-
-			System.out.println("\nDigite um número do teclado:");
-
-			numerodoteclado = leia.nextInt();
-
-		}
-
-		if (numerodoteclado % 3 == 0) {
-
-			soma = numerodoteclado + soma;
-
-			contanumeros++;
-
+		do {
+		System.out.println("\nDigite um número do teclado:");
+		numerodoteclado = leia.nextInt();
+				if (numerodoteclado % 3 == 0 && numerodoteclado != 0) {
+				soma = numerodoteclado + soma;
+				contanumeros++;
+			}
+			}	
+			while (numerodoteclado != 0);
+			
+		if (contanumeros != 0) {
 			media = soma / contanumeros;
-
 			System.out.println("\nA média dos números múltiplos de 3 é: " + media);
-
 			media = leia.nextInt();
 
 		}
 
 		else {
-
 			System.out.println("\nOs números digitados não são múltiplos de 3.");
-
 		}
 
 	}
 
 }
+
+
